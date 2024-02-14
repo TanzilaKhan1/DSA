6621977a24ada2a2cfc1a76644a87aad36848724
TanzilaKhan1 <149196723+TanzilaKhan1@users.noreply.github.com>
2024-02-14 12:39:20 +0600

    Update counting_sort.cpp

--- counting_sort.cpp
+++ counting_sort.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 
 int main(){
-    int upper,lower,n;
-    scanf("%d %d %d",&lower,&upper,&n);
+    double high,low;
+    int n,digit;
+    scanf("%lf %lf %d %d",&low,&high,&n,&digit);
+    int upper = (int)(high * digit);
+    int lower = (int)(low * digit);
     int add,r;
     if(lower<0){
       add= abs(lower);
@@ -25,24 +28,51 @@ int main(){
         arr[i]=0;
     }
     
+    double number;
     int num,ind;
     for (int i = 0; i < n; i++)
     {
-        scanf("%d",&num);
+        scanf("%lf",&number);
+        num= (int)(number*digit);
         ind=num+add;
         arr[ind]+=1;
     }
     
     for (ind = 0; ind < r+1; ind++)
     {
-        if(arr[ind]!=0){
-           num=ind-add;
-           for (int i = 0; i < arr[ind]; i++)
-           {
-               printf("%d ",num);
-           } 
+        int ans;
+        if(arr[ind]>0){
+          num=ind-add;
+          number=(double)num/digit;
+          ans = (int)(number * digit);  //digit=10000
         }
+      
+        while(arr[ind]>0)
+        {   
+                if (ans % 10000 == 0)
+                {
+                    printf("%.0lf\n", number);
+                }
+                else if (ans % 1000 == 0)
+                {
+                    printf("%.1lf\n", number);
+                }
+                else if (ans % 100 == 0)
+                {
+                    printf("%.2lf\n", number);
+                }
+                else if (ans % 10 == 0)
+                {
+                    printf("%.3lf\n", number);
+                }
+                else
+                {
+                    printf("%.4lf\n", number);
+                }
+          arr[ind]--;
+        } 
     }
+
     printf("\n");
     return 0;
 }
