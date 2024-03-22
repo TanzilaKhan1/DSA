bd2e4b3e0d832b69b5557d9a238cb91a98ef67ae
TanzilaKhan1 <149196723+TanzilaKhan1@users.noreply.github.com>
2024-03-22 12:50:13 +0600

    Update combination.cpp

--- allcombination.cpp
+++ allcombination.cpp
@@ -1,51 +1,32 @@
 #include<bits/stdc++.h> 
 using namespace std; 
  
-/* arr[] ---> Input Array 
-data[] ---> Temporary array to store current combination 
-start & end ---> Starting and Ending indexes in arr[] 
-index ---> Current index in data[] 
-r ---> Size of a combination to be printed */
-void combinationUtil(int arr[], int data[], 
-                    int start, int end, 
-                    int index, int r) 
+int r;
+
+void combinations(int arr[], int data[], 
+                    int start, int end, int ind) 
 { 
-    // Current combination is ready
-    // to be printed, print it 
-    if (index == r) 
+    if (ind == r) 
     { 
-        for (int j = 0; j < r; j++) 
-            cout << data[j] << " "; 
-        cout << endl; 
+        printf("%d",data[0]);
+        for (int j = 1; j < r; j++) 
+            printf(" %d",data[j]); 
+        printf("\n"); 
         return; 
     } 
  
-    // replace index with all possible 
-    // elements. The condition "end-i+1 >= r-index"
-    // makes sure that including one element 
-    // at index will make a combination with 
-    // remaining elements at remaining positions 
-    for (int i = start; i <= end && 
-        end - i + 1 >= r - index; i++) 
+    for (int i = start; i <= end && (end - i + 1) >= (r - ind); i++) 
     { 
-        data[index] = arr[i]; 
-        combinationUtil(arr, data, i+1, 
-                        end, index+1, r); 
+        data[ind] = arr[i]; 
+        combinations(arr, data, i+1, end, ind+1); 
     } 
 } 
 
-
-void printCombination(int arr[], int N, int R) 
-{ 
-    // A temporary array to store all combination one by one 
-    int data[R]; 
-    combinationUtil(arr, data, 0, N-1, 0, R); 
-} 
- 
 int main() 
 { 
-    int arr[] = {1, 2, 3, 4, 5}; 
-    int R = 3; 
-    int N = sizeof(arr)/sizeof(arr[0]); 
-    printCombination(arr, N, R); 
+    int arr[] = {1, 2, 3, 4, 5,6}; 
+    r = 4; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    int data[r];
+    combinations(arr, data, 0, n-1, 0);
 } 
