74cbfb1e790b99233e145c7e8b8bff6c959cff00
TanzilaKhan1 <149196723+TanzilaKhan1@users.noreply.github.com>
2024-03-22 13:08:24 +0600

    Update allpermutations.cpp

--- allpermutations.cpp
+++ allpermutations.cpp
@@ -1,34 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to print permutations of string with duplicates allowed
-// This function takes three parameters:
-// 1. String
-// 2. Starting index of the string
-// 3. Ending index of the string.
-void permute(string a, int start, int end)
-{
-    
-    if (start == end)
+void permutations(string a, int start, int end)
+{   
+    if (start == end){
         cout << a << endl;
+        return;}
     else
-    {
-        // Permutations made
+    {       
         for (int i = start; i <= end; i++)
         {
-            swap(a[start], a[end]);
+            swap(a[start], a[i]);
             permute(a, start + 1, end);
-
-            // backtrack
-            swap(a[start], a[end]);
+            swap(a[start], a[i]);
         }
     }
 }
 
 int main()
 {
-    string str = "ABC";
+    string str = "ABCD";
     int n = str.size();
-    permute(str, 0, n - 1);
+    permutations(str, 0, n - 1);
     return 0;
 }
