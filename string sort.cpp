0506b6e7d1566032c0d8488308cdb843d9c70770
TanzilaKhan1 <149196723+TanzilaKhan1@users.noreply.github.com>
2024-03-24 00:25:32 +0600

    Update string sort.cpp

--- string sort.cpp
+++ string sort.cpp
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-int length(char a[]){
-  int l;
-  for (l = 0; l < 100000; l++){
-      if(a[l]=='\0'){break;}
-    }
-    return l;
-}
-
 int lexicographical(char s1[],char s2[],int l1){
   int flag=0;
      for (int i = 0; i <= l1; i++){
@@ -35,57 +27,4 @@ void swapstring(int flag, char s1[], char s2[]){
         if(temp[i]=='\0'){break;}
       }
     }
-}
-
-
-char ind[100];
-void compare_two_String(char x[], char y[], int w, int u, int v){
-    for (int c = 0; c < 100; c++)
-    {
-        int temp;
-        if (x[c] == '\0'){
-            break;
-        }
-        if (y[c] == '\0'){
-            w = -1;
-        }
-        if (x[c] > y[c]) {
-            w = -1;
-        }
-        else if (x[c] < y[c]){
-            break;
-        }
-        else{
-            continue;
-        }
-        if (w = -1){
-            temp = ind[u];
-            ind[u] = ind[v];
-            ind[v] = temp;
-            break;
-        }
-    }
-}
-
-int main(){
-    char str[100][100];
-    int n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++){
-        scanf("%s", str[i]);
-    }
-    int total = n, flag = 0;
-    for (int j = 0; j < total; j++){
-        ind[j] = j;
-    }
-    for (int m = 0; m < total - 1; m++){
-        for (int k = m + 1; k < total; k++){
-            compare(str[m], str[k], flag, m, k);
-        }
-    }
-    for (size_t i = 0; i < total; i++){
-        printf("%d ", ind[i]);
-    }
-    int x = 15;
-    printf("%d ", !x);
 }
