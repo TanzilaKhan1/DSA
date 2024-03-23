cfa5f949839a784c765bb41cb113f9bc755497f5
TanzilaKhan1 <149196723+TanzilaKhan1@users.noreply.github.com>
2024-03-23 12:31:34 +0600

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,47 +1,48 @@
 #include <iostream>
 using namespace std;
 
-void merge(int arr[], int start, int mid, int end) {
-  int n1 = mid - start + 1;
-  int n2 = end - mid;
-  int L[n1], R[n2];
-
-  for (int i = 0; i < n1; i++)
-    L[i] = arr[start + i];
-  for (int j = 0; j < n2; j++)
-    R[j] = arr[mid + 1 + j];
-  
-  int i=0, j=0, k=start;
-  while (i < n1 && j < n2) {
-    if (L[i] <= R[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = R[j];
-      j++;
+void merge(int arr[], int left[], int leftSize, int right[], int rightSize, int l) {
+    int i = 0, j = 0, k = l;
+    while (i < leftSize && j < rightSize) {
+        if (left[i] <= right[j]) {
+            arr[k] = left[i];
+            k++;
+            i++;
+        } else {
+            arr[k] = right[j];
+            k++;
+            j++;
+        }
     }
-    k++;
-  }
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
-  while (j < n2) {
-    arr[k] = R[j];
-    j++;
-    k++;
-  }
+    while (i < leftSize) {
+        arr[k] = left[i];
+        k++;
+        i++;
+    }
+    while (j < rightSize) {
+        arr[k] = right[j];
+        k++;
+        j++;
+    }
 }
 
 void mergeSort(int arr[], int l, int r) {
-  if (l < r) {
-    int m = l + (r - l) / 2;
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
-    merge(arr, l, m, r);
-  }
+    if (l >= r) {
+        return;
+    }
+    int mid = l + (r - l) / 2;
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid + 1, r);
+    int left[mid - l + 1], right[r - mid];
+    for (int i = 0; i <= mid - l; i++) {
+        left[i] = arr[l + i];
+    }
+    for (int i = 0; i < r - mid; i++) {
+        right[i] = arr[mid + 1 + i];
+    }
+    merge(arr, left, mid - l + 1, right, r - mid, l);
 }
+
 
 void printArray(int arr[], int size) {
   for (int i = 0; i < size; i++)
