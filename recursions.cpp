e2a4a1e6ec97f807bcf354efa44e807e70c113f2
TanzilaKhan1 <149196723+TanzilaKhan1@users.noreply.github.com>
2024-03-23 23:59:52 +0600

    Update recursions.cpp

--- recursions.cpp
+++ recursions.cpp
@@ -59,45 +59,6 @@ int bsearch(int start,int end,int search,char x[]){
   }
 }
 
-int length(char a[]){
-  int l;
-  for (l = 0; l < 100000; l++)
-    {
-      if(a[l]=='\0'){break;}
-    }
-    return l;
-}
-int lexicographical(char s1[],char s2[],int l1){
-  int flag=0;
-     for (int i = 0; i <= l1; i++)
-     {
-      if(s1[i]>s2[i]){ flag=-1;
-      break;}
-      if(s1[i]<s2[i]){flag=1;
-      break;}
-     }
-     return flag;
-}
-void swapstring(int flag, char s1[], char s2[]){
-    char temp[100000];
-    if(flag==-1){
-      for (int i = 0; i < 100000; i++)
-      {
-        temp[i]=s1[i];
-        if(s1[i]=='\0'){break;}
-      }
-      for (int i = 0; i < 100000; i++)
-      {
-        s1[i]=s2[i];
-        if(s2[i]=='\0'){break;}
-      }
-      for (int i = 0; i < 100000; i++)
-      {
-        s2[i]=temp[i];
-        if(temp[i]=='\0'){break;}
-      }
-    }
-}
 int gcd(int n1,int n2){
  int temp;
  if(n1==n2){return n1;}
