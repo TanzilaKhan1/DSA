d2d8daee3539859c5a677e9af555b2bd216a2b5b
TanzilaKhan1 <149196723+TanzilaKhan1@users.noreply.github.com>
2024-03-29 01:22:54 +0600

    Update median_of_sorted_array.cpp

--- median_of_sorted_array.cpp
+++ median_of_sorted_array.cpp
@@ -1,36 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-double findMedianSortedArrays(const vector<int>& nums1, const vector<int>& nums2) {
-    if (nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
+double findMedianSortedArrays(int arr1[],int len1,int arr2[],int len2) {
+    if (len1 > len2) 
+         return findMedianSortedArrays(arr2, len1, arr1,len2);
 
-    int x = nums1.size();
-    int y = nums2.size();
-    int low = 0, high = x;
+    int low = 0, high = len1;
     while (low <= high) {
-        int partitionX = (low + high) / 2;
-        int partitionY = (x + y + 1) / 2 - partitionX;
+        int part1 = (low + high) / 2;
+        int part2 = (len1+ len2+ 1) / 2 - part1;
+        int maxL1 ,minR1,maxL2,minR2;
+        
+        if(part1 == 0){
+            maxL1=INT_MIN;
+        }
+        else{
+            maxL1=arr1[part1 - 1];
+        }
+        if(part1 == len1){
+            minR1=INT_MAX;
+        }
+        else{
+            minR1=arr1[part1];
+        }
+        
+        if(part2 == 0){
+            maxL2=INT_MIN;
+        }
+        else{
+            maxL2=arr2[part2 - 1];
+        }
+        if(part2 == len2){
+            minR2=INT_MAX;
+        }
+        else{
+            minR2=arr2[part2];
+        }
 
-        int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
-        int minRightX = (partitionX == x) ? INT_MAX : nums1[partitionX];
 
-        int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
-        int minRightY = (partitionY == y) ? INT_MAX : nums2[partitionY];
-
-        if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-            if ((x + y) % 2 == 0) {
-                return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
-            } else {
-                return max(maxLeftX, maxLeftY);
+        if (maxL2 <= minR1 && maxL1 <= minR2) {
+            if ((len1 + len2) % 2 != 0) {
+                
+                return max(maxL1, maxL2);
+            } 
+            else {
+                return (max(maxL1, maxL2) + min(minR1, minR2)) / 2.0;
             }
-        } else if (maxLeftX > minRightY) {
-            high = partitionX - 1;
+        } else if (maxL1 <= minR2) {
+            low = part1 + 1;
         } else {
-            low = partitionX + 1;
+            high = part1 - 1;
         }
     }
-
 }
+
+int main() {
+    int arr1[] = {1, 3};
+    int arr2[] = {2, 3 ,4};
+    int len1=2;
+    int len2=3;
+    cout << findMedianSortedArrays(arr1,len1,arr2,len2) << endl;
+    return 0;
+}
+
 
 int main() {
     vector<int> nums1 = {1, 3};
