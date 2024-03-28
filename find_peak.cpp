d2c0fb98c55524a3de95b33aa6660c27400540ca
TanzilaKhan1 <149196723+TanzilaKhan1@users.noreply.github.com>
2024-03-29 01:22:15 +0600

    Update find_peak.cpp

--- find_peak.cpp
+++ find_peak.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h> 
 using namespace std;
 
-int findPeakElement(const vector<int>& nums) {
-    int low = 0, high = nums.size() - 1;
+int findPeakElement(int arr[],int n) {
+    int low = 0, high = n - 1;
     while (low < high) {
         int mid1 = (low + high) / 2;
         int mid2 = mid1 + 1;
-        if (nums[mid1] < nums[mid2]) {
-            low = mid2;
-        } else {
+        if (arr[mid1] >= arr[mid2]) {
             high = mid1;
+        } else {
+            low = mid2;
         }
     }
     return low;
 }
 
 int main() {
-    vector<int> nums = {1, 2, 1, 3, 5, 6, 4};
-    cout <<findPeakElement(nums) << endl;
+    int arr[]= {1, 2, 3, 4, 5, 6,};
+    int len=6;
+    cout <<findPeakElement(arr,len) << endl;
     return 0;
 }
