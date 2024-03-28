2558a3db140aa2a635cccbec97be5647f6a7eb5d
TanzilaKhan1 <149196723+TanzilaKhan1@users.noreply.github.com>
2024-03-29 01:23:53 +0600

    Update number_of_rotations.cpp

--- number_of_rotations.cpp
+++ number_of_rotations.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h> 
 using namespace std;
-int findRotationCount(const vector<int>& nums) {
-    int low = 0, high = nums.size() - 1;
+int findRotationCount(int arr[],int len) {
+    int low = 0, high = len - 1;
     while (low < high) {
         int mid = low + (high - low) / 2;
-        if (nums[mid] > nums[high]) {
-            low = mid + 1;
-        } else {
+        if (arr[high] >= arr[mid]) { 
             high = mid;
+        } else {
+            low = mid + 1;
         }
     }
     return low;
 }
 
-int main() {
-    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
-    cout << findRotationCount(nums) << endl;
+int main() {   //right rotation
+    int arr[] = {4, 5, 6, 7, 0, 1, 2};
+    int len=7;
+    cout << findRotationCount(arr,len) << endl;
     return 0;
 }
