d9c23df2e2090004ee247766be98f71c9bd4fb22
TanzilaKhan1 <149196723+TanzilaKhan1@users.noreply.github.com>
2024-03-29 17:10:30 +0600

    Update balanced_parentheses.cpp

--- balanced_parentheses.cpp
+++ balanced_parentheses.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-
 
 void print_arr(char arr[],int len){
     for (size_t i = 0; i < len; i++)
@@ -11,23 +9,22 @@ void print_arr(char arr[],int len){
     printf("\n");
 }
 
-
-void generateParenthesis(int open, int close, char arr[],int ind) {
+void makeParenthesis(char arr[],int ind, int open, int close) {
     if(open == 0 && close == 0) {
         print_arr(arr,ind);
         return;
     }
     if(open > 0) {
         arr[ind]='(';
-        generateParenthesis(open-1, close,arr, ind+1);}
+        makeParenthesis(arr, ind+1, open-1, close);}
     if(close > open) {
         arr[ind]=')';
-        generateParenthesis(open, close-1, arr, ind+1);}
+        makeParenthesis(arr, ind+1, open, close-1);}
 }
 
 int main() {
-    int n = 4; // 3 pairs of parentheses  
+    int n = 4;  
     char arr[2*n];
-    generateParenthesis(n, n, arr,0);
+    makeParenthesis(arr,0, n, n );
     return 0;
 }
